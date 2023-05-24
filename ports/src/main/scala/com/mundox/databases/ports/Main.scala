889a1782e39c1d9e9b9b16eb851a44e553d675e2
Milenio X <jnkmiloq@gmail.com>
2023-05-23 21:38:08 -0500

    Main implementation to call the jdbc function and execute all the CRUD operations over Users table

--- ports/src/main/scala/com/mundox/databases/ports/Main.scala
+++ ports/src/main/scala/com/mundox/databases/ports/Main.scala
@@ -2,9 +2,71 @@ package com.mundox.databases.ports
 
 import com.mundox.databases.core.domain.entities.User
 import com.mundox.databases.core.domain.valueobjects.{Id, Email}
+import com.mundox.databases.ports.config.Environment
 
-object Main extends App {
-  println("Hello World")
-  val user: User = User(Id("6ab24d67-736f-4d7c-9e87-9e2d37c6a83b"),"user1","John","Doe",Email("johndoe@example.com"),"rl23qBm9Uh6u4eDcNz1i")
-  println(s"USer $user")
+import java.util.UUID
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("Starting service to connect Scala with databases")
+    val env = createEnvironment()
+
+    println("SEARCHING...")
+    val getResult = env.userQueries.getUsers
+    getResult.onComplete {
+      case Failure(exception) =>
+        println("There was an error retrieving the users information", exception)
+      case Success(users) =>
+        println("The list of users is the following:")
+        println(users)
+    }
+
+    println("DELETING...")
+    val deleteRes = env.userCommand.deleteUser(Id("2ef3dae3-6e12-4b32-9739-80a29d2ed574"))
+    deleteRes.onComplete {
+      case Failure(exception) =>
+        println("There was an error deleting the new user", exception)
+      case Success(_) => {
+        println(s"User with id 2ef3dae3-6e12-4b32-9739-80a29d2ed574 was deleted")
+      }
+    }
+
+    println("ADDING...")
+    var id: Id = Id("Empty")
+    val insertRes = env.userCommand.addUser {
+      val id: Id = Id(UUID.randomUUID().toString)
+      User(id, "user00", "HELLO", "WORLD", Email("example@domain.com"), "abc")
+    }
+    insertRes.onComplete {
+      case Failure(exception) =>
+        println("There was an error adding the new user", exception)
+      case Success(user) => {
+        id = Id(user.id.value)
+        println(s"Added the user with id ${user.id.value}")
+      }
+    }
+
+    println("SEARCHING ONE...")
+    val getUserRes = env.userQueries.getUserById(id)
+    getUserRes.onComplete {
+      case Failure(exception) =>
+        println("There was an error adding the new user", exception)
+      case Success(user) => user match {
+        case Some(u) =>
+          println("The user is the following:")
+          println(u)
+        case None =>
+          println(s"The user with the id ${id.value} was not found in the data")
+      }
+    }
+
+    Thread.sleep(20000)
+  }
+
+
+
+  private def createEnvironment():Environment = new Environment
 }
