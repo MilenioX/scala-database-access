7a9c7ed8fa00ac7e64535d0bfa55bfcac37d9550
Milenio X <jnkmiloq@gmail.com>
2023-05-23 22:25:27 -0500

    Added Configuration file and classes to load properties

---
+++ ports/src/main/scala/com/mundox/databases/ports/config/DefaultConfigurations.scala
@@ -0,0 +1,18 @@
+package com.mundox.databases.ports.config
+
+object DefaultConfigurations {
+
+  def getDefaultConfigurations: Config = {
+    Config(
+      JDBCConfig(
+        "jdbc:postgresql://",
+        "org.postgresql.Driver",
+        "localhost",
+        5432,
+        "postgres",
+        "postgres",
+        "example")
+    )
+  }
+
+}
